53133c8ea23ca0759ac75037bf8751350439b688
Chaz Simons <chaz.simons@gmail.com>
2021-08-31 17:23:50 -0700

    Test: test for code creation

--- lib/game.rb
+++ lib/game.rb
@@ -16,26 +16,5 @@ class Game
     end
   end
 
-  def cheat_code
-    puts secret
-  end
-
-  def announcement
-    puts "I have generated a beginner sequence with four elements made up of: (r)ed,
-          (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
-          What's your guess?"
-    puts '>'
-    user_input = gets.chomp
-    user_input.upcase!
-    user_input.split(//)
-    if user_input.length < 4 || user.length > 4
-      puts "Please enter a 4 letter combo of the above colors."
-    else
-      compare_input
-    end
-  end
-
-  def compare_input
-
 
 end
